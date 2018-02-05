93385696979c5c8272f1daf725aac4b40bf4583e
Nydia Segura <nydia.segura@gmail.com>
2018-02-04 18:50:25 -0600

    delete button and removed logo

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import uuid from 'uuid';
 
 export class ToDoList extends Component {
   constructor(props){
@@ -8,13 +8,15 @@ export class ToDoList extends Component {
     this.state = {
       tasks:[]
     }
+    this.deleteTask = this.deleteTask.bind(this)
   }
 
 
 addTask(){
   let tasks = this.state.tasks;
   tasks.push({
-    title:this.inputElement.value
+    title:this.inputElement.value,
+    id:uuid.v4()
   })
 
   this.setState({
@@ -23,15 +25,26 @@ addTask(){
   this.inputElement.value=""
 }
 
+
+deleteTask(index){
+  let newArray = this.state.tasks
+  newArray.splice(index,1)
+
+  this.setState({
+    tasks:newArray
+  })
+
+}
+
   render() {
     return (
       <div className="container">
       <div className="row">
       <div className="col-md-6 offset-3">
         <h1>To Do List </h1>
-        <input type="text" ref={(a)=> this.inputElement=a} placeholder="Enter your task" required/>
+        <input type="text" ref={(a)=> this.inputElement=a} placeholder="Enter your task"/>
         <button className="btn btn-info btn-sm" onClick={this.addTask}>Add</button>
-        <ToDoListItem tasks={this.state.tasks}/>
+        <ToDoListItem tasks={this.state.tasks} onDelete={this.deleteTask}/>
         </div>
         </div>
       </div>
@@ -39,21 +52,11 @@ addTask(){
   }
 }
 
-export class ToDoListItem extends Component {
-  constructor(props){
-    super(props)
-    this.deleteTask = this.deleteTask.bind(this)
-  }
 
-  deleteTask(){
-    let newArray = this.props.tasks
-    newArray.map(function (index) {
-      console.log(index)
-    })
-  }
+export class ToDoListItem extends Component {
 
   render() {
-    let task = this.props.tasks.map((item)=> <li className="item_style list-group-item justify-content-between">{item.title}<button className="btn btn-info" onClick={this.deleteTask}>Delete</button></li>)
+    let task = this.props.tasks.map((item,index)=> <li className="item_style list-group-item justify-content-between">{item.title}<input type="hidden" value={item.id}/><button className="btn btn-info" onClick={()=>this.props.onDelete(index)}>Delete</button></li>)
     return(
       <div className="card">
         <ul className="list-group">
