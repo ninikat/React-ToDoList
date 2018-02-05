31cc6b9108df1e61af62410d2174d4d3541497be
Nydia Segura <nydia.segura@gmail.com>
2018-02-05 08:59:24 -0600

    added pending/completed functionality

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -6,9 +6,13 @@ export class ToDoList extends Component {
     super(props)
     this.addTask = this.addTask.bind(this)
     this.state = {
-      tasks:[]
+      tasks:[],
+      completed:[]
     }
     this.deleteTask = this.deleteTask.bind(this)
+    this.moveTask = this.moveTask.bind(this)
+    this.completeDeleteTask = this.completeDeleteTask.bind(this)
+    this.switchTask = this.switchTask.bind(this)
   }
 
 
@@ -29,22 +33,70 @@ addTask(){
 deleteTask(index){
   let newArray = this.state.tasks
   newArray.splice(index,1)
+  this.setState({
+    tasks:newArray
+  })
+}
+
+completeDeleteTask(index){
+  let newArray = this.state.completed
+  newArray.splice(index,1)
+  this.setState({
+    completed:newArray
+  })
+}
+
+moveTask(index){
+  let newArray = this.state.tasks
+  let completedArray = this.state.completed
+  let title = this.state.tasks[index].title
+  let id = this.state.tasks[index].id
+  newArray.splice(index,1)
 
   this.setState({
     tasks:newArray
   })
+  completedArray.push({
+    title:title,
+    id: id
+  })
+  this.setState({
+    completed:completedArray
+  })
+}
 
+switchTask(index){
+  let newArray = this.state.tasks
+  let completedArray = this.state.completed
+  let title = this.state.completed[index].title
+  let id = this.state.completed[index].id
+  console.log(title)
+  completedArray.splice(index,1)
+
+  this.setState({
+   completed:completedArray
+  })
+
+  newArray.push({
+    title:title,
+   id: id
+      })
+
+  this.setState({
+    tasks:newArray
+  })
+  console.log(completedArray)
 }
 
   render() {
     return (
       <div className="container">
       <div className="row">
-      <div className="col-md-6 offset-3">
-        <h1>To Do List </h1>
+      <div className="col-md-6 offset-2">
+        <h1 class="mt-3">To Do List </h1>
         <input type="text" ref={(a)=> this.inputElement=a} placeholder="Enter your task"/>
-        <button className="btn btn-info btn-sm" onClick={this.addTask}>Add</button>
-        <ToDoListItem tasks={this.state.tasks} onDelete={this.deleteTask}/>
+        <button className="btn btn-info btn-sm" onClick={this.addTask}>Add</button><p></p>
+        <ToDoListItem tasks={this.state.tasks} completed={this.state.completed} onDelete={this.deleteTask} onCompleteDelete={this.completeDeleteTask} onComplete={this.moveTask} onIncomplete={this.switchTask}/>
         </div>
         </div>
       </div>
@@ -56,12 +108,32 @@ deleteTask(index){
 export class ToDoListItem extends Component {
 
   render() {
-    let task = this.props.tasks.map((item,index)=> <li className="item_style list-group-item justify-content-between">{item.title}<input type="hidden" value={item.id}/><button className="btn btn-info" onClick={()=>this.props.onDelete(index)}>Delete</button></li>)
+    let task = this.props.tasks.map((item,index)=> <li className="item_style list-group-item justify-content-between">{item.title}
+    <input type="hidden" value={item.id}/>
+    <button className="btn btn-info btn-sm" onClick={()=>this.props.onComplete(index)}>Mark as Completed</button>
+    <button className="btn btn-danger btn-sm" onClick={()=>this.props.onDelete(index)}>Delete</button></li>)
+
+    let completed = this.props.completed.map((item,index)=> <li className="item_style list-group-item justify-content-between">{item.title}
+    <input type="hidden" value={item.id}/>
+    <button className="btn btn-info btn-sm" onClick={()=>this.props.onIncomplete(index)}>Mark as Pending</button>
+    <button className="btn btn-danger btn-sm" onClick={()=>this.props.onCompleteDelete(index)}>Delete</button></li>)
+
+
+
     return(
       <div className="card">
+        <div>
+        <h4 className="p-2">Pending Items </h4>
         <ul className="list-group">
       {task}
         </ul>
+        </div>
+        <div>
+        <h4 className="p-2">Completed Items </h4>
+        <ul className="list-group">
+        {completed}
+        </ul>
+        </div>
       </div>
     )
   }
